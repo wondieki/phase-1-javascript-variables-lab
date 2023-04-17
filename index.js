81bc8abae2bd1b4046ca0516ed4f96370bc8a96c
Wilder <wkerubo28@gmail.com>
2023-04-17 12:22:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const companyName = "Scuber";
 
-let mostProfitableNeighborhood = "Chelsea"
 
-let companyCeo ="Susan Smith"
+function saturdayFun(doThis="roller-skate") {
+    return `This Saturday, I want to ${doThis}!`
+    }
+    saturdayFun()
+    
+    const mondayWork = function(goHere="go to the office"){
+     return `This Monday, I will ${goHere}.` 
+    }
+    mondayWork()
+
+    function wrapAdjective (one="%") {
+        return function(stringOne="you are a human"){
+          return `You are ${one}${stringOne}${one}!`
+        }
+    }
